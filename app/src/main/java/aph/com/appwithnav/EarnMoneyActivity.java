f5110b1a1d1088f025975b30032969bafd9e8a1b
Amit <amit.prag@sap.com>
2018-04-18 10:09:56 +0530

    First Commit

---
+++ app/src/main/java/aph/com/appwithnav/EarnMoneyActivity.java
@@ -0,0 +1,137 @@
+package aph.com.appwithnav;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import aph.com.appwithnav.model.Student;
+import butterknife.OnClick;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.reward.RewardItem;
+import com.google.android.gms.ads.reward.RewardedVideoAd;
+import com.google.android.gms.ads.reward.RewardedVideoAdListener;
+
+import android.util.Log;
+import android.widget.Toast;
+
+public class EarnMoneyActivity extends AppCompatActivity implements RewardedVideoAdListener {
+
+    private RewardedVideoAd mRewardedVideoAd;
+    private Student st;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        Log.i("earn activity","createdon");
+        //MobileAds.initialize(this, "ca-app-pub-6876139477910913~6213074548");
+        MobileAds.initialize(this, "ca-app-pub-3940256099942544/5224354917");
+
+        mRewardedVideoAd = MobileAds.getRewardedVideoAdInstance(this);
+        mRewardedVideoAd.setRewardedVideoAdListener(this);
+        loadRewardedVideoAd();
+        Log.i("earn activity","done-------------cosnt");
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_earn_money);
+    }
+
+    private void loadRewardedVideoAd() {
+
+/*
+        mRewardedVideoAd.loadAd("ca-app-pub-6876139477910913~6213074548",
+                new AdRequest.Builder().build());
+*/
+        mRewardedVideoAd.loadAd("ca-app-pub-3940256099942544/5224354917",
+                new AdRequest.Builder().build());
+    }
+
+    /*@OnClick(R.id.show_ads)
+    public void showAd()
+    {Log.i("earn_activity","show ads clicked");
+        if (rewardedVideoAd != null && rewardedVideoAd.isAdLoaded()) {
+            rewardedVideoAd.show();
+        }
+
+    }*/
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+
+    }
+
+    public void showads(View view) {
+        Log.i("earn_activity","show ads clicked");
+
+        if (mRewardedVideoAd.isLoaded()) {
+            mRewardedVideoAd.show();
+        }
+        else
+        {
+            loadRewardedVideoAd();
+        }
+    }
+
+    @Override
+    public void onRewarded(RewardItem reward) {
+        Toast.makeText(this, "onRewarded! currency: " + reward.getType() + "  amount: " +
+                reward.getAmount(), Toast.LENGTH_SHORT).show();
+        // Reward the user.
+    }
+
+    @Override
+    public void onRewardedVideoAdLeftApplication() {
+        Toast.makeText(this, "onRewardedVideoAdLeftApplication",
+                Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onRewardedVideoAdClosed() {
+        loadRewardedVideoAd();
+        Toast.makeText(this, "onRewardedVideoAdClosed", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onRewardedVideoAdFailedToLoad(int errorCode) {
+        Log.i("earn_activity_error","Failed to load ad");
+        Toast.makeText(this, "onRewardedVideoAdFailedToLoad", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onRewardedVideoAdLoaded() {
+        Toast.makeText(this, "onRewardedVideoAdLoaded", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onRewardedVideoAdOpened() {
+        Toast.makeText(this, "onRewardedVideoAdOpened", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onRewardedVideoStarted() {
+        Toast.makeText(this, "onRewardedVideoStarted", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onRewardedVideoCompleted() {
+        Toast.makeText(this, "Congratulations!! You have earned 10 credit coins", Toast.LENGTH_SHORT).show();
+    }
+    @Override
+    public void onResume() {
+        mRewardedVideoAd.resume(this);
+        super.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        mRewardedVideoAd.pause(this);
+        super.onPause();
+    }
+
+    @Override
+    public void onDestroy() {
+        mRewardedVideoAd.destroy(this);
+        super.onDestroy();
+    }
+
+
+
+}
---
+++ app/src/main/java/aph/com/appwithnav/quesFeedActivity.java
@@ -0,0 +1,13 @@
+package aph.com.appwithnav;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class quesFeedActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.question_feed);
+    }
+}
